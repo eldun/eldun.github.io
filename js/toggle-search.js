1c654ab7e328c8f04c00edfe5ad08fc611b4843f
evan <dunneev@gmail.com>
2020-05-18 02:39:32 -0400

    Improve search look & feel

--- js/toggle-search.js
+++ js/toggle-search.js
@@ -1,9 +1,12 @@
 function toggleSearch() {
     var x = document.getElementById("search-container");
+    var y = document.getElementById("search-button");
+    var z = document.getElementById("search-input");
 
-    if (x.style.visibility === "hidden") {
+
+    if (x.style.visibility == "hidden") {
       x.style.visibility = "visible";
-      document.getElementById("search-input").focus();
+      z.focus();
 
       document.onkeydown = function(evt) {
         evt = evt || window.event;
@@ -15,5 +18,15 @@ function toggleSearch() {
 
     } else {
       x.style.visibility = "hidden";
+    }
+
+    if (x.style.visibility == "visible"){
+      y.style.color = "#42cad1"
+      x.style.display = "block";
+      z.focus();
+    }
+    else {
+      y.style.color = "#cc773f";
+      x.style.display = "none";
     }
   }
