74eb9edaabaab2b4d9729c63eda93087e04412b4
evan <dunneev@gmail.com>
2022-09-29 14:25:50 -0400

    Add script to generate live site

---
+++ scripts/update-live-site.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+ 
+ 
+if [ $(basename $PWD) != eldun.github.io ]
+then
+    exit "Please execute 'update-live-site.sh' from the site's root directory"
+fi
+ 
+git checkout source
+ 
+# Generate site from branch 'source'
+bundle exec jekyll build
+ 
+git checkout master
+ 
+# Move all generated files in _site to root directory 
+mv _site/* .
+ 
+git push
+
+git checkout source
